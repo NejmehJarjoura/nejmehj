b2f623f186579b9ea038169544dc239b351c9924
Nejmeh Jarjoura <nejmehjarjoura@gmail.com>
2020-08-15 10:11:58 +0300

    Fade in animation change

--- js/myscripts.js
+++ js/myscripts.js
@@ -48,9 +48,9 @@ function check_if_in_view_slide_left() {
       //check to see if this current container is within viewport
       if ((element_bottom_position >= window_top_position) &&
           (element_top_position <= window_bottom_position)) {
-        $element.addClass('animate__animated animate__zoomIn');
+        $element.addClass('animate__animated animate__fadeInDown');
       } else {
-        $element.removeClass('animate__animated animate__zoomIn');
+        $element.removeClass('animate__animated animate__fadeInDown');
       }
     });
   }
@@ -117,43 +117,3 @@ function check_if_in_view_slide_left() {
       }
     });
   }
-// $(window).scroll(function(){
-//     if($(window).scrollTop() >= $("#about").offset().top + $("#about").height() 
-//         || $(window).scrollTop() < $("#about").offset().top - ($("#about").height()/2)) {
-
-//         $('#about-title').removeClass('animate__animated animate__slideInLeft');
-//         $('#download-button').removeClass('animate__animated animate__slideInLeft');
-//     } else {
-//         $('#about-title').addClass('animate__animated animate__slideInLeft');
-//         $('#download-button').addClass('animate__animated animate__slideInLeft');
-//     }
-
-//     if($(window).scrollTop() >= $("#services").offset().top + $("#services").height() 
-//         || $(window).scrollTop() < $("#services").offset().top - ($("#services").height()/2)) {
-
-//         $('#services-card1').removeClass('animate__animated animate__slideInLeft');
-//         $('#services-card2').removeClass('animate__animated animate__slideInDown');
-//         $('#services-card3').removeClass('animate__animated animate__slideInRight');
-//     } else {
-//         $('#services-card1').addClass('animate__animated animate__slideInLeft');
-//         $('#services-card2').addClass('animate__animated animate__slideInDown');
-//         $('#services-card3').addClass('animate__animated animate__slideInRight');
-//     }
-
-//     if($(window).scrollTop() >= $("#portfolio").offset().top + $("#portfolio").height() 
-//         || $(window).scrollTop() < $("#portfolio").offset().top - ($("#portfolio").height()/2)) {
-
-//         $('.portfolio-card').removeClass('animate__animated animate__zoomIn');
-//     } else {
-//         $('.portfolio-card').addClass('animate__animated animate__zoomIn');
-//     }
-
-//     if($(window).scrollTop() >= $("#contact").offset().top + $("#contact").height() 
-//         || $(window).scrollTop() < $("#contact").offset().top - ($("#contact").height()/2)) {
-
-//         $('.social-div').removeClass('animate__animated animate__lightSpeedInRight');
-//     } else {
-//         $('.social-div').addClass('animate__animated animate__lightSpeedInRight');
-//     }
-        
-// });
